7776c28ea5577be17e78e4f9f0a4a07a47a4a89f
akshay-movieverse <67101074+akshay-movieverse@users.noreply.github.com>
2021-03-13 19:02:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -50,30 +50,23 @@ class Two(Resource):
     @cross_origin(origin='*')
     
     def get(self,name):
-        try:
-            browser = load_chrome_driver()
-            wait = ui.WebDriverWait(browser, 5)
-            url="https://animixplay.to/v1/"+name
-            browser.get(url)
-            iframe = wait.until(lambda browser: browser.find_element_by_id("iframeplayer"))
-            tv=browser.switch_to.frame(iframe)
-            try:
-                wait.until(lambda browser: browser.find_element_by_css_selector("#videocontainer > div > div.plyr__video-wrapper > video > source"))
-                html = browser.page_source
-                soup = BeautifulSoup(html,'html.parser')
-                st=soup.findAll('video' )#.getText()
-                yu=st[0].find("source")['src']
+        
+        browser = load_chrome_driver()
+        wait = ui.WebDriverWait(browser, 5)
+        url="https://animixplay.to/v1/"+name
+        browser.get(url)
+        iframe = wait.until(lambda browser: browser.find_element_by_id("iframeplayer"))
+        tv=browser.switch_to.frame(iframe)
 
-            except:
-                wait.until(lambda browser: browser.find_element_by_id("player_html5_api"))
-                html = browser.page_source
-                soup = BeautifulSoup(html,'html.parser')
-                st=soup.findAll('video' )#.getText()
-                yu=st[0]['src']
+        wait.until(lambda browser: browser.find_element_by_css_selector("#videocontainer > div > div.plyr__video-wrapper > video > source"))
+        html = browser.page_source
+        soup = BeautifulSoup(html,'html.parser')
+        st=soup.findAll('video' )#.getText()
+        yu=st[0].find("source")['src']
 
-            return {'link': str(yu)}
-        except:
-            return 'fail'
+
+        return {'link': str(yu)}
+
 
 
 api.add_resource(One, "/4Anime/<string:name>")
