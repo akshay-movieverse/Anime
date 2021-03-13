0d14595773420ab78c0a90ba93a0f601e3e50634
akshay-movieverse <67101074+akshay-movieverse@users.noreply.github.com>
2021-03-13 17:25:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask,jsonify, request 
 from flask_restful import Api, Resource
-
+from selenium import webdriver
 import cloudscraper as cfscrape
 from bs4 import BeautifulSoup
-
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import ui
 from flask_cors import CORS, cross_origin
-import json
-from json import JSONEncoder
-import pafy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
@@ -15,14 +13,15 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/api3": {"origins": "*"}})
 api = Api(app)
 
-class Three(Resource):
+class One(Resource):
     @cross_origin(origin='*')
     def get(self,name):
         try:
-            scraper = cfscrape.create_scraper()
-            curl="https://4anime.to/" + name
-            data=(scraper.get(curl).content)
-            soup = BeautifulSoup(data,'html.parser')
+            browser = webdriver.PhantomJS()
+            url="https://4anime.to/"+name
+            browser.get(url)
+            html = browser.page_source
+            soup = BeautifulSoup(html,'html.parser')
             st=soup.findAll('video' )#.getText()
             for gt in st:
                 yu=gt.find('source')
@@ -32,8 +31,39 @@ class Three(Resource):
         except:
             return 'fail'
 
+class Two(Resource):
+    @cross_origin(origin='*')
+    def get(self,name):
+        try:
+            browser = webdriver.Chrome(options=options)
+            wait = ui.WebDriverWait(browser, 5)
+            url="https://animixplay.to/v1/"+name
+            browser.get(url)
+            iframe = wait.until(lambda browser: browser.find_element_by_id("iframeplayer"))
+            tv=browser.switch_to.frame(iframe)
+            try:
+                wait.until(lambda browser: browser.find_element_by_css_selector("#videocontainer > div > div.plyr__video-wrapper > video > source"))
+                html = browser.page_source
+                soup = BeautifulSoup(html,'html.parser')
+                st=soup.findAll('video' )#.getText()
+                yu=st[0].find("source")['src']
+
+            except:
+                wait.until(lambda browser: browser.find_element_by_id("player_html5_api"))
+                html = browser.page_source
+                soup = BeautifulSoup(html,'html.parser')
+                st=soup.findAll('video' )#.getText()
+                yu=st[0]['src']
+
+            return {'link': str(yu)}
+        except:
+            return 'fail'
 
 
-api.add_resource(Three, "/4Anime/<string:name>")
+api.add_resource(One, "/4Anime/<string:name>")
+api.add_resource(Two, "/Animix/<string:name>")
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
